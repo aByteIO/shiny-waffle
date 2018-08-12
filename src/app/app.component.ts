288bd3ddfc91902f5741276c6f9765cbcbda842a
Trong Nguyen <trong.nguyen@xappido.com>
2018-08-12 10:00:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngsif-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'shiny-waffle';
+}
